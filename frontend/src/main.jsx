002280fcc6e2622eef50349699a39dddba2c4d38
Risha Yadav <rishaa.who@gmail.com>
2026-06-12 16:42:38 +0530

    Fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <h1>HELLO WORLD</h1>
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
